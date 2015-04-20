4742e9d7781352e4b82b2bc7b10603b9b7d60e8b
benaryorg <binary@benary.org>
2015-04-20 14:44:33 +0200

    add second exercise

---
+++ exercise2.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+source ./functions.sh
+
+printusage()
+{
+	echo "usage: $0 [-h] [-o file] [-d directory]"
+}
+
+printhelp()
+{
+	printusage
+	cat << "END"
+
+        -h --help       this message
+        -o              output zip file
+        -d              input directory
+
+Saves all Java source files in your home directory (or the provided directory)
+into a zip file.
+END
+}
+
+DIRECTORY=~
+OUTPUTFILE=javabackup.$(date +%0d-%0m-%0Y).tar
+
+while [ ! -z "$1" ];do
+	case "$1" in
+		"-h"|"--help")
+			printhelp
+			exit 0
+		;;
+		"-d")
+			shift
+			if [ -z "$1" ];then
+				printusage
+			fi
+			DIRECTORY="$1"
+		;;
+		"-o")
+			shift
+			if [ -z "$1" ];then
+				printusage
+			fi
+			OUTPUTFILE="$1"
+		;;
+		*)
+			printusage
+			exit 1
+		;;
+	esac
+	shift
+done
+
+FILES=$(find $DIRECTORY -regex '.+\.java' -mtime -8)
+tar -cf $FILES $OUTPUTFILE > /dev/null 2>&1
+
