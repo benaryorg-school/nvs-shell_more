69c92583de17e01ea9d483adbf4ffbd0775d0c71
benaryorg <binary@benary.org>
2015-04-20 13:48:39 +0200

    add first exercise

---
+++ exercise1.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+source ./functions.sh
+
+printusage()
+{
+	echo "usage: $0 [-h] string ..."
+}
+
+printhelp()
+{
+	printusage
+	cat << "END"
+
+        -h --help       this message
+END
+}
+
+if [ -z "$*" ];then
+	printusage
+	exit 1
+fi
+
+for i in "$@";do
+	if [ "$i" == "-h" ] || [ "$i" == "--help" ];then
+		printhelp
+		exit 0
+	fi
+done
+
+for i in "$@";do
+	printf "$i\t\t\t"
+	if ! isint "$i";then
+		printf "not "
+	fi
+	echo "numeric"
+done
+
